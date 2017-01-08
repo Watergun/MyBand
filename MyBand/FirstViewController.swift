fa0e7cfab30f3c53331c36f86d6cb7fdce1102ea
Mac User <macuser@macs-mac-mini.local>
2017-01-09 00:31:55 +0100

    Initial Commit

---
+++ MyBand/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyBand
+//
+//  Created by Mac User on 09.01.17.
+//  Copyright © 2017 MyBand Project. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
